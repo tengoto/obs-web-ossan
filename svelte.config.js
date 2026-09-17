f3d7f6b40d5a2ddca1f304cb3c038ebc4e22c24d
tengoto <ten10goto@gmail.com>
2026-09-17 17:19:36 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
 import adapter from '@sveltejs/adapter-static'
-import { sveltekit } from '@sveltejs/kit/vite'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import purgecssImport from '@fullhuman/postcss-purgecss'
-import { defineConfig } from 'vite'
+
+export const viteConfig = {
+  plugins: [],
+}
 
 const config = {
   preprocess: vitePreprocess(),
+
   kit: {
+    paths: {
+      base: '/obs-web-ossan'
+    },
+
     adapter: adapter({
       pages: 'public',
       assets: 'public',
       fallback: 'index.html'
     }),
+
     output: {
       bundleStrategy: 'single'
     }
@@ -19,42 +26,3 @@ const config = {
 }
 
 export default config
-
-export const viteConfig = defineConfig(({ mode }) => {
-  const production = mode === 'production'
-  const purgecss = purgecssImport.default ?? purgecssImport
-
-  return {
-    appType: 'custom',
-    plugins: [sveltekit()],
-    css: {
-      postcss: {
-        plugins: [
-          production &&
-            purgecss({
-              content: [
-                './src/**/*.svelte',
-                './src/**/*.js',
-                './src/**/*.ts',
-                './src/**/*.html',
-                './src/app.html'
-              ],
-              safelist: {
-                standard: [/svelte-/]
-              },
-              variables: true
-            })
-        ].filter(Boolean)
-      }
-    },
-    build: {
-      minify: 'terser',
-      cssMinify: 'lightningcss',
-      terserOptions: {
-        compress: {
-          passes: 2
-        }
-      }
-    }
-  }
-})
